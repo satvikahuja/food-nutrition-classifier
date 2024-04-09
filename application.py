43e246910c734dd04859caf789fdf71c709cefaa
Satvik Ahuja <109898261+satvikahuja@users.noreply.github.com>
2024-04-09 10:32:57 +0530

    Update application.py

--- application.py
+++ application.py
@@ -97,9 +97,9 @@ def generate_frames():
 def index():
     return render_template('index.html')
 
-@appliation.route('/video')
+@application.route('/video')
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    application.run(debug=True, threaded=True)
